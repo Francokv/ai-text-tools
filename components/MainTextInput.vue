01b4912297275dd660d9a4690bd0cea09ea62cba
Franco Vallejos <fvallejosx@gmail.com>
2024-11-15 19:51:24 -0300

    initialize Nuxt.js project with basic configuration and components

---
+++ components/MainTextInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <textarea
+    v-model="text"
+    placeholder="Type something here"
+    class="w-full h-32 p-2 border border-gray-300 rounded-lg"
+  >
+
+  </textarea>
+</template>
+
+<script setup>
+const text = ref('hola')
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <div
+    class="mx-10 my-10"
+  >
+    <MainTextInput/>
+  </div>
+</template>
+
+<script>
+
+</script>
